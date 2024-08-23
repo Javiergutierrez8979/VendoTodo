874abefb4de2740790d2b06f00463a30bb035285
JavierGutierrez <javiergutierrez8979@gmail.com>
2024-08-22 23:42:25 -0300

    Prueba3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/VendoTodo/', // Cambia esto por tu nombre de repositorio
 });
