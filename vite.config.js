a73e7c626fe1193d034cb50ed63a6e022f716740
JavierGutierrez <javiergutierrez8979@gmail.com>
2024-08-31 15:08:55 -0300

    31824dos

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Cambia esto por tu nombre de repositorio
+  build: {
+    rollupOptions: {
+      external: ['@fortawesome/react-fontawesome']
+    }
+  }
 });
